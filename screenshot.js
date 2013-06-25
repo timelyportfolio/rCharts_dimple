68ed3c06bfef8985aeb777aba093de17ce94beb8
timelyportfolio <kent.russell@timelyportfolio.com>
2013-06-25 13:26:43 -0500

    get casperjs working

--- screenshot.js
+++ screenshot.js
@@ -1,7 +1,17 @@
-var casper = require('casper').create();
+var casper = require('casper').create({viewportSize:{width:1600,height:900}});
+
+//casper.start("gallery/example01_bar_vert.html", function() {
+//    this.capture('example.png', {
+//        top: 0,
+//        left: 0,
+//        width: 1024,
+//        height: 768
+//    });
+//});
 
 casper.start('gallery/example11_bar_horiz_stack_grp.html', function() {
-this.captureSelector('example11.png', '.container');
+this.captureSelector('example11.png', '.rChart');
 });
+
 
 casper.run();
