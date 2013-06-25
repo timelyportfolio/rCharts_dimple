2bd0eb8d91b6996a7957bec710c569e65671e9dd
timelyportfolio <kent.russell@timelyportfolio.com>
2013-06-25 13:07:19 -0500

    try to add casperjs screenshot script
    but get blank png except for the very first run

--- screenshot.js
+++ screenshot.js
@@ -1,7 +1,7 @@
 var casper = require('casper').create();
 
-casper.start('http://timelyportfolio.github.io/rCharts_dimple/gallery/example11_bar_horiz_stack_grp.html', function() {
-    this.captureSelector('example11.png', '.rChart');
+casper.start('gallery/example11_bar_horiz_stack_grp.html', function() {
+this.captureSelector('example11.png', '.container');
 });
 
 casper.run();
