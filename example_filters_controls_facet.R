dc56b62174d4b0598fe7dc6560162619eaf8491f
timelyportfolio <kent.russell@timelyportfolio.com>
2014-03-07 12:45:27 -0600

    allow margin specification through rCharts

--- example_filters_controls_facet.R
+++ example_filters_controls_facet.R
@@ -45,3 +45,42 @@ d1
 
 d1$addAssets("./js/d3-grid.js")
 d1$save("filter_controls_facet_single.html",cdn=T)
+
+
+
+
+
+
+d1 <- dPlot(
+  Sex~Freq,
+  groups = c("Eye"),
+  data = as.data.frame(HairEyeColor),
+  type = "bubble",
+  height = 400,
+  width = 600,
+  margins = list(left=80,top=40,right=40,bottom=40)
+)
+d1$params$facet = list(x=NULL,y=NULL)
+d1$addControls(
+  "facetx",
+  value = "Hair",
+  values = colnames(as.data.frame(HairEyeColor)[-4])
+)
+d1$addControls(
+  "y",
+  value = "Sex",
+  values = colnames(as.data.frame(HairEyeColor)[-4])
+)
+d1$addControls(
+  "groups",
+  value = "Eye",
+  values = colnames(as.data.frame(HairEyeColor)[-4])
+)
+d1$addFilters("Hair","Eye")
+d1$defaultColors("#!d3.scale.category10()!#")
+d1$xAxis(type = "addMeasureAxis")
+d1$yAxis(type ="addCategoryAxis")
+d1$templates$script = 
+  "./chart_singleselect.html"
+  #"http://timelyportfolio.github.io/rCharts_dimple/chart_singleselect.html"
+d1
