af272d437276ddc308cc9bba46e8145d67636155
timelyportfolio <kent.russell@timelyportfolio.com>
2014-04-11 22:57:47 -0500

    more for issue 381 correlation plot; biggest is add custom tooltip

--- example_rCharts_issue381.R
+++ example_rCharts_issue381.R
@@ -32,5 +32,61 @@ corrmatplot$colorAxis(
   type = "addColorAxis"
   ,colorSeries = 'CorrelationCoefficient'
   ,palette = c('red','white','blue')
+  ,outputFormat = "0.5f"
+)
+corrmatplot
+
+
+#now do the bar
+corrmatplot$set(type = "bar")
+corrmatplot
+
+
+#with the bar let's play with tooltips
+#first tooltip will just be the number in the center of the bar
+corrmatplot$templates$script = 
+  "http://timelyportfolio.github.io/rCharts_dimple/chart_tooltip_flexible.html"
+
+#this template is designed to let us define onHover and onLeave
+#with thought afterScript but does not work since separate <script> block
+#so use chartDiv instead
+corrmatplot$setTemplate(
+  chartDiv = 
+'
+<div id = "{{chartId}}"></div>
+<script>
+function onHover(e){
+  //ugly but it works; if tooltip exists then select otherwise append
+  //whole d3 enter, update, exit makes things difficult here
+  var custTool = (
+    d3.select(e.selectedShape[0][0].parentNode).select("#chartTooltip")[0][0] ? 
+    d3.select(e.selectedShape[0][0].parentNode).select("#chartTooltip") :
+    d3.select(e.selectedShape[0][0].parentNode).append("text").attr("id","chartTooltip")
+  )
+    
+  custTool
+    //reads outputFormat and assumes it exists
+    .text(d3.format(opts.colorAxis.outputFormat)(e.selectedShape.data()[0].cValue))
+    //turn dispay on with css since none from leave
+    .style("display",null)
+    .style("pointer-events","none")
+    //use x and y from selected rectangle to position with transform for center
+    .attr("x",e.selectedShape.attr("x"))
+    .attr("y",e.selectedShape.attr("y"))
+    //move to center
+    .attr("transform",
+      "translate(" + e.selectedShape.attr("width") / 2 + "," + e.selectedShape.attr("height")/2 + ")"
+    )
+    .attr("dy",6)
+    .style("text-anchor","middle")
+}
+function onLeave(e){
+  myChart.svg.select("#chartTooltip")
+    .style("display","none");
+}
+</script>
+'
+,
+afterScript = '<script></script>'
 )
 corrmatplot
